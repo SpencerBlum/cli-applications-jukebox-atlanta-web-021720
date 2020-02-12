7c4f4e6f496d82ddc9bf7735ce6d61f730bf34c8
Spencer Blum <spencerblum@gmail.com>
2020-02-12 23:36:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,20 @@
 # Add your code here
+def help
+puts "I accept the following commands:"
+puts "- help : displays this help message"
+puts "- list : displays a list of songs you can play"
+puts "- play : lets you choose a song to play"
+puts "- exit : exits this program"
+end
+
+def list(songs)
+  i = 0
+  array = []
+  for i in 0...songs.length do
+    puts "#{i+1}. #{songs[i]}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
